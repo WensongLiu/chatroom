c8506a047fc4893666d3412f1286a5c9befb8935
Wensong Liu <lws.19940712@gmail.com>
2019-05-04 10:15:01 -0400

    get and  post method undated

--- server.js
+++ server.js
@@ -16,5 +16,21 @@ app.use(bodyParser.urlencoded({extended: false}))
 var dbUrl = 'mongodb://localhost:27017/chatroom';
 
 mongoose.connect(dbUrl, {useNewUrlParser: true});
-var Message = mongoose.model(‘Message’,{ name : String, message : String})
+var Message = mongoose.model('Message',{ name : String, message : String});
 
+//back-end get method
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
+});
+
+//back-end post method
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) =>{
+    if(err)
+      sendStatus(500);
+    res.sendStatus(200);
+  })
+});
