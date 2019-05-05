2c7292c4d2e3d24c5a83791183f8478289e490bc
Wensong Liu <lws.19940712@gmail.com>
2019-05-05 15:42:42 -0400

    added socket.io

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var http = require('http');
+var socketio = require('socket.io');
 
 var app = express();
-
-var server = app.listen(3000, () => {
-    console.log('Server is running on the port:', server.address().port);
-  });
+var server = http.Server(app);
+var io = socketio(server);
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -15,8 +15,10 @@ app.use(bodyParser.urlencoded({extended: false}))
 //connect to MongoDB
 var dbUrl = 'mongodb://localhost:27017/chatroom';
 
-mongoose.connect(dbUrl, {useNewUrlParser: true});
-var Message = mongoose.model('Message',{ name : String, message : String});
+var Message = mongoose.model('Message',{ 
+  name : String, 
+  message : String
+});
 
 //back-end get method
 app.get('/messages', (req, res) => {
@@ -31,6 +33,18 @@ app.post('/messages', (req, res) => {
   message.save((err) =>{
     if(err)
       sendStatus(500);
+    io.emit('message', req.body);
     res.sendStatus(200);
   })
 });
+
+//create connection
+io.on('connection', () =>{
+  //console.log('A user connected!');
+})
+
+mongoose.connect(dbUrl, {useNewUrlParser: true});
+
+server = server.listen(3000, () => {
+    console.log('Server is running on the port:', server.address().port);
+  });
